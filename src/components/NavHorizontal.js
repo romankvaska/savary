459b85d276dcc36bc521345298b9b52371b9e807
romankvaska <41154023+romankvaska@users.noreply.github.com>
2021-04-30 10:52:44 +0800

    Add files via upload
    Third commit

---
+++ src/components/NavHorizontal.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo.svg';
+import '../css/menu.css';
+
+class NavHorizontal extends Component {
+    render() {
+        const { isAuthenticated, login, logout } = this.props.auth;
+
+        return (
+            <nav className="row menuheader">
+                <div className="col-md-5">
+                    <Link to="/">
+                        <img src={logo} className="logo" alt="Logo" />
+                        SAVARY
+                    </Link>
+                     - Your Personal Budgeting Online Solution
+                </div>
+                <div className="col-md-7 menuheaderright">
+                    SEARCH |&nbsp;
+                    { isAuthenticated() ? (
+                            <>
+                                <Link to="/profile">User Profile</Link> | <Link onClick={logout}>Log Out</Link>
+                            </>
+                        ) : (
+                            <Link to="/" onClick={login}>Sign In/Up</Link>
+                        ) 
+                    }
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavHorizontal;
