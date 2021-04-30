eb06d3ba9332cbfd9e33803e3ac14c721c2b336d
romankvaska <41154023+romankvaska@users.noreply.github.com>
2021-04-30 11:39:45 +0800

    Cleaning stylesheets by moving files into dedicated CSS folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/main.css';
 import App from './components/app';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
